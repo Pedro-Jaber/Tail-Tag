352f9b169236e9e1d632dc645fd2115ed5dc3969
Pedro Jaber <pedrojaberc@gmail.com>
2024-10-30 16:03:57 -0300

    feat(model_user): add `email`, `password`,`address` to user information

--- model/model_user.js
+++ model/model_user.js
@@ -1,10 +1,31 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema(
   {
+    email: {
+      type: String,
+      required: [true, "Please enter an email"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      minlength: [6, "Minimum password length is 6 characters"],
+    },
     name: String,
     cpf: String,
+    address: {
+      street: String,
+      number: String,
+      city: String,
+      state: String,
+      country: String,
+      zip_code: String,
+    },
     pets: [
       {
         petId: mongoose.Schema.Types.ObjectId,
@@ -16,3 +37,4 @@ const UserSchema = new Schema(
 );
 
 module.exports = mongoose.model("User", UserSchema);
+// List possible informations to be stored in user
