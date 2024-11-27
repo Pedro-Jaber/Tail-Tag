1662316f34be43105f09c61799f2216160a513d3
Pedro Jaber <pedrojaberc@gmail.com>
2024-11-26 23:23:16 -0300

    feat(pet model): add `collar` object to `PetSchema` and move `latitude` and `longitude` to `gpsData` object

--- model/model_pet.js
+++ model/model_pet.js
@@ -5,8 +5,15 @@ const PetSchema = new Schema(
   {
     name: String,
     birthdate: Date,
-    latitude: [mongoose.Schema.Types.Decimal128],
-    longitude: [mongoose.Schema.Types.Decimal128],
+    collar: {
+      serialNumber: String,
+      version: String,
+    },
+    gpsData: {
+      time: [mongoose.Schema.Types.Date],
+      latitude: [mongoose.Schema.Types.Decimal128],
+      longitude: [mongoose.Schema.Types.Decimal128],
+    },
   },
   { timestamps: true, collection: "pets" }
 );
