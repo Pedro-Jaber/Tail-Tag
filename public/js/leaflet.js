8beb25d90594974e681febc6181270c6bb1be2c7
Pedro Jaber <pedrojaberc@gmail.com>
2024-11-26 23:56:10 -0300

    fix(leaflet): fix wrong reference for latitude and longitude `json.latitude` --> `json.gpsData.latitude`

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -19,15 +19,15 @@ async function get_pet_position(pet_id) {
       return response.json();
     })
     .then((json) => {
-      console.log(json);
+      //console.log(json);
       // console.log(json.latitude.reverse()[0]);
       // console.log(json.latitude.reverse()[0].$numberDecimal);
       // console.log(json.longitude.reverse()[0].$numberDecimal);
 
       // Target's GPS coordinates.
       let target = L.latLng(
-        json.latitude.reverse()[0].$numberDecimal,
-        json.longitude.reverse()[0].$numberDecimal
+        json.gpsData.latitude.reverse()[0].$numberDecimal,
+        json.gpsData.longitude.reverse()[0].$numberDecimal
       );
 
       // Place a marker on the same location.
